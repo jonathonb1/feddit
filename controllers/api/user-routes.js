88a1b80e89ccff29fe24541b839f2af1670fb4b2
Ryan <ryanpjhickey@gmail.com>
2022-10-08 20:53:24 -0700

    add !validPassword if statement

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -31,6 +31,12 @@ router.post('/login', (req, res) => {
         // Verify user
 
         const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
 
     });
 });
