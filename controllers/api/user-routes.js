914019929c627ea8b2f021b2abddaa71d78a2b38
Huy <drac0123451@gmail.com>
2022-10-10 10:50:09 -0700

    added comment.js model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,5 +2,46 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
 const withAuth = require('../../utils/auth');
+<<<<<<< Updated upstream
+=======
+
+module.exports = router;
+
+router.put('/:id', withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+});
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        res.json({ user: dbUserData });
+
+        // Verify user
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+    });
+});
+>>>>>>> Stashed changes
 
 module.exports = router;
