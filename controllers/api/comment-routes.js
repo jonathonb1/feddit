0d9a850bc4676029ba9f912f11a80dbf265d1a20
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-10-12 08:49:53 -0700

    routes added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// get all comments
 router.get('/', (req, res) => {
   Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
@@ -12,7 +11,6 @@ router.get('/', (req, res) => {
     });
 });
 
-// create a commment
 router.post('/', withAuth, (req, res) => {
   // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
   Comment.create({
@@ -27,7 +25,6 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-// delete a comment
 router.delete('/:id', withAuth, (req, res) => {
   Comment.destroy({
     where: {
