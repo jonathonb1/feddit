0490ab38c85e488a658da716da2ab7f9d61d753c
Huy <drac0123451@gmail.com>
2022-10-11 21:02:55 -0700

    add route to find all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,6 +4,17 @@ const withAuth = require('../../utils/auth');
 
 module.exports = router;
 
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 router.put('/:id', (req, res) => {
     User.update(req.body, {
         individualHooks: true,
