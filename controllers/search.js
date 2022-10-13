bc1a0dc8649d359d8d8934759459670c29233d4b
Ryan <ryanpjhickey@gmail.com>
2022-10-13 09:41:07 -0700

    add search.js in controllers and search.handlebars

---
+++ controllers/search.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
+const withAuth = require('../utils/auth');
+
+function searchButton() {
+
+    const searchVal = document.querySelector('.searchVal').value.trim();
+
+    router.get('/post?title=' & searchVal), (req, res) => {
+        console.log('======================');
+        Post.findAll({
+            // where: {
+            //     postdata.title.value.includes(searchVal)
+            // },
+            attributes: [
+                'id',
+                'post_content',
+                'title',
+                'created_at',
+                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+            .then(dbPostData => {
+                const posts = dbPostData.map(post => post.get({ plain: true }));
+
+                res.render('search', {
+                    posts,
+                    loggedIn: req.session.loggedIn
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    };
+
+}
+
+document.querySelector('.searchBtn').addEventListener('submit', searchButton)
